eb6ad04188dc5f15b2f727509452692256996186
Tom Broad <tombroadyt@gmail.com>
2022-06-24 13:55:41 +0100

    pagination working and error handling introduced

--- src/components/Applications/ApplicationServices/index.jsx
+++ src/components/Applications/ApplicationServices/index.jsx
@@ -1,11 +1,11 @@
 
 const url = 'http://localhost:3001/api/applications'
 
-export function getApplications(pageNumber, limitNumber) {
+export const getApplications = (pageNumber, limitNumber) =>{
   return fetch(`${url}?_page=${pageNumber}&_limit=${limitNumber}`, {
     method: 'GET',
     headers: {'Content-Type': 'application/json'},
   })
     .then(response => response.json())
-    .catch(err => console.error(err))
+    .catch(err => err);
 }
--- src/components/Applications/Applications.jsx
+++ src/components/Applications/Applications.jsx
@@ -1,25 +1,41 @@
 import React, { useState, useEffect } from "react";
 import SingleApplication from "./SingleApplication/SingleApplication";
 import { getApplications } from "./ApplicationServices";
+import { Button } from "../../ui/Button/Button";
 import styles from "./Applications.module.css";
 
 const Applications = () => {
 
+  const count = 5;
   const [applications, setApplications] = useState([])
-  const [applicationNumber, setApplicationNumber] = useState(1);
-  
+  let applicationNumber = 1;
+
+  //helper function to handle errors in getting applications
+  const checkApplication = (applicationNumber, count) => {
+    return getApplications(applicationNumber, count)
+      .then(response => {
+        if(response.length) {
+          setApplications([...applications, ...response]);
+        }
+        else alert('There has been an error with the request')
+      })
+  }
 
   //on inital page load make a call to the api to return first 5 applications
   useEffect(() => {
-    getApplications(applicationNumber, 5)
-      .then(response => setApplications(...applications, response))
+    checkApplication(applicationNumber, count)
   }, [])
 
   //onClick return the next page number by increasing the counter by one
+  const handleClick = () => {
+    applicationNumber++
+    checkApplication(applicationNumber, count)
+  }
 
   return (
     <div className={styles.Applications}>
-      {applications.map(application => <SingleApplication application={application} />)}
+      {applications.length && applications.map(application => <SingleApplication key={application.email} application={application} />)}
+      {applications.length && <Button onClick={handleClick} />}
     </div>
   );
 };
