4e401d4bbdcf3436708a0b1eba056a320c072934
Tom Broad <tombroadyt@gmail.com>
2022-06-24 12:35:16 +0100

    inital applications loading

--- src/components/Applications/Applications.jsx
+++ src/components/Applications/Applications.jsx
@@ -1,13 +1,25 @@
-import React from "react";
-import SingleApplication from "../SingleApplication/SingleApplication";
-import { getSingleApplicationFixture } from "../../__fixtures__/applications.fixture";
+import React, { useState, useEffect } from "react";
+import SingleApplication from "./SingleApplication/SingleApplication";
+import { getApplications } from "./ApplicationServices";
 import styles from "./Applications.module.css";
 
 const Applications = () => {
-  const applications = getSingleApplicationFixture;
+
+  const [applications, setApplications] = useState([])
+  const [applicationNumber, setApplicationNumber] = useState(1);
+  
+
+  //on inital page load make a call to the api to return first 5 applications
+  useEffect(() => {
+    getApplications(applicationNumber, 5)
+      .then(response => setApplications(...applications, response))
+  }, [])
+
+  //onClick return the next page number by increasing the counter by one
+
   return (
     <div className={styles.Applications}>
-      <SingleApplication application={applications[0]} />
+      {applications.map(application => <SingleApplication application={application} />)}
     </div>
   );
 };
